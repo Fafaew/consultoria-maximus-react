509d125269e02a61403f58b453e4499cd35ef04b
fafaew <rafael.gomest@gmail.com>
2024-01-11 16:40:46 -0300

    adeed dropdown in navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { FaBars, FaTimes, FaInstagram, FaLinkedin } from "react-icons/fa";
 import styles from './Navbar.module.scss';
 import logo from '../../Assets/Img/logo.png';
+import { RiArrowDropDownLine } from "react-icons/ri";
 
 
 export default function Navbar() {
   const navRef = useRef();
   const navOverlay = useRef();
+
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => setDropdownOpen(!isDropdownOpen);
 
   const showNavBar = () => {
     navRef.current.classList.toggle(`${styles.responsiveNav}`)
@@ -15,9 +20,12 @@ export default function Navbar() {
   }
 
   const closeNavBar = () => {
-    navRef.current.classList.remove(`${styles.responsiveNav}`)
-    navOverlay.current.classList.remove(`${styles.navOverlay}`)
+    navRef.current.classList.remove(`${styles.responsiveNav}`);
+    navOverlay.current.classList.remove(`${styles.navOverlay}`);
+    hideDropdown();
   }
+
+  const hideDropdown = () => setDropdownOpen(false);
 
   return (
     <div className={styles.overlay} ref={navOverlay}>
@@ -26,7 +34,15 @@ export default function Navbar() {
         <nav ref={navRef} className={styles.nav}>
           <ul className={styles.navHeaderList}>
             <Link to="/" onClick={closeNavBar}>HOME</Link>
-            <Link to="/about" onClick={closeNavBar}>QUEM SOMOS</Link>
+            <li
+              className={`${styles.dropdownItem} ${isDropdownOpen ? styles.open : ''}`}
+            >
+              <Link onClick={toggleDropdown}>QUEM SOMOS <RiArrowDropDownLine className={styles.dropdownArrow} /> </Link>
+              <ul className={styles.dropdown}>
+                <li><Link to="/about/team" onClick={closeNavBar}>Equipe</Link></li>
+                <li><Link to="/about/history" onClick={closeNavBar}>História</Link></li>
+              </ul>
+            </li>
             <Link to="/services" onClick={closeNavBar}>SERVIÇOS</Link>
             <Link to="/contact" onClick={closeNavBar}>CONTATO</Link>
           </ul>
@@ -36,7 +52,7 @@ export default function Navbar() {
                 <a href="http://www.instragram.com"><FaInstagram /></a>
               </button>
               <button className={styles.socialLinksButton}>
-              <a href="http://www.linkedin.com"><FaLinkedin /> </a>
+                <a href="http://www.linkedin.com"><FaLinkedin /> </a>
               </button>
             </div>
             <img className={styles.logoNavOverlay} src={logo} alt="Logo Maximus"/>
