571184db28bbe91281ef5a9eee04ac7b53ae3f67
fafaew <rafael.gomest@gmail.com>
2024-03-19 19:52:24 -0300

    Added clients

--- src/Routes/Clients/Clients.jsx
+++ src/Routes/Clients/Clients.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import React from 'react';
+import styles from './Clients.module.scss';
+import companyLogo from '../../Assets/Img/ClientsLogo/companyLogo.png'
+
 
 const Clients = () => {
   return (
-    <div>Clients</div>
+    <div className={styles.clientsContainer}>
+      <div className={styles.title}>Clientes</div>
+
+      <div className={styles.clientsWrapper}>
+        <div className={styles.clientLogo}>
+          <img className={styles.companyImage} src={companyLogo} alt="Logo Empresa"/>
+        </div>
+
+        <div className={styles.clientLogo}>
+          <img className={styles.companyImage} src={companyLogo} alt="Logo Empresa"/>
+        </div>
+
+        <div className={styles.clientLogo}>
+          <img className={styles.companyImage} src={companyLogo} alt="Logo Empresa"/>
+        </div>
+
+        <div className={styles.clientLogo}>
+          <img className={styles.companyImage} src={companyLogo} alt="Logo Empresa"/>
+        </div>
+
+        <div className={styles.clientLogo}>
+          <img className={styles.companyImage} src={companyLogo} alt="Logo Empresa"/>
+        </div>
+
+        <div className={styles.clientLogo}>
+          <img className={styles.companyImage} src={companyLogo} alt="Logo Empresa"/>
+        </div>
+
+        <div className={styles.clientLogo}>
+          <img className={styles.companyImage} src={companyLogo} alt="Logo Empresa"/>
+        </div>
+
+        <div className={styles.clientLogo}>
+          <img className={styles.companyImage} src={companyLogo} alt="Logo Empresa"/>
+        </div>
+
+        <div className={styles.clientLogo}>
+          <img className={styles.companyImage} src={companyLogo} alt="Logo Empresa"/>
+        </div>
+
+        <div className={styles.clientLogo}>
+          <img className={styles.companyImage} src={companyLogo} alt="Logo Empresa"/>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
